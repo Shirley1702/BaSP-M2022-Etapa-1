2ed021597887f0563292e7ab27dc2a939e85ec47
shirley1702 <shirleyseaton17@gmail.com>
2022-04-26 22:44:06 -0300

    BaSP-W06-2022:checkpoint-n°2.Login finished

---
+++ Semana-06/javascript/login.js
@@ -0,0 +1,54 @@
+window.onload = function () {
+    var form = document.getElementsByClassName('form-login');
+    var email = document.getElementById('email');
+    var errorEmail = document.getElementsByClassName('error-Email');
+
+    //emial validation
+
+    var email = document.getElementById('email');
+    var errorEmail = document.getElementsByClassName('error-email');
+
+    email.addEventListener('blur', emailBlur);
+    email.addEventListener('focus', emailFocus);
+
+    function emailBlur() {
+        var validEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+        if (email.value === '') {
+            errorEmail[0].textContent = 'please enter a email';
+        }
+        else if (!validEmail.test(email.value)) {
+            errorEmail[0].textContent = 'please enter a valid email';
+        }
+    }
+    function emailFocus() {
+        errorEmail[0].textContent = '';
+    }
+
+    //password validation
+
+    var password = document.getElementById('password');
+    var errorPassword = document.getElementsByClassName('error-password')
+
+    password.addEventListener('blur', passwordBlur);
+    password.addEventListener('focus', passwordFocus);
+
+    function passwordBlur() {
+        if (password.value === '') {
+            errorPassword[0].textContent = 'please enter your password';
+        }
+        else if (password.value.length < 8) {
+            errorPassword[0].textContent = '8 or more characters with numbers and letters';
+        }
+        else {
+            for (i = 0; i < password.value.length; i++) {
+                if (!number.includes(password.value[i]) && !letters.includes(password.value[i])) {
+                    errorPassword[0].textContent = 'requires numbers and letters';
+                }
+            }
+        }
+    }
+    function passwordFocus() {
+        errorPassword[0].textContent = '';
+    }
+
+}
